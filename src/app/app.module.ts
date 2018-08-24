acc3442aad55a9bd7999a7bae6121bd5883be066
Davecar Grave <davecargrave@gmail.com>
2018-08-25 02:22:16 +0800

    contact form initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { WorkingPageComponent } from './pages/working-page/working-page.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'working-page', component: WorkingPageComponent },
   // {
   //   path: 'heroes',
@@ -34,11 +38,15 @@ const appRoutes: Routes = [
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    SkillsComponent
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+
+  isSubmitted = false;
+
+  dataLink = 'https://script.google.com/macros/s/AKfycbwmt_znXdUF-3qIL2FLZ57gfrsOh9rCSU7GWZpWK5gvQ5L5bdo/exec';
+
+
+  contact = this.fb.group({
+    name: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+    email: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.email])],
+    message: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+  });
+
+
+  ngOnInit() {
+    const node = document.createElement('iframe');
+    node.setAttribute('name', 'hidden_iframe');
+    node.setAttribute('src', this.dataLink);
+    node.style.display = 'none';
+    document.body.appendChild(node);
+  }
+
+  onSubmit() {
+    console.log('submit');
+    this.isSubmitted = this.isSubmitted ? false : true;
+  }
+
+}
